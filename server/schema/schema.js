96d967981533aca930ccac88dc451f44c687fe62
Jonas <jonasqalolino@gmail.com>
2022-09-09 12:24:01 +0800

    chore: add Project model

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,6 @@
-const { projects, clients } = require('../sampleData');
+// Mongoose Models
+const Project = require('../models/Project');
+const Client = require('../models/Client');
 
 const {
 	GraphQLObjectType,
@@ -19,8 +21,7 @@ const ProjectType = new GraphQLObjectType({
 		client: {
 			type: ClientType,
 			resolve(parent, args) {
-				console.log('projectType', { parent });
-				return clients.find((client) => client.id === parent.clientId);
+				return Client.findById(parent.clientId);
 			},
 		},
 	}),
@@ -43,28 +44,27 @@ const RootQuery = new GraphQLObjectType({
 		projects: {
 			type: new GraphQLList(ProjectType),
 			resolve(parent, args) {
-				return projects;
+				return Project.find();
 			},
 		},
 		project: {
 			type: ProjectType,
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
-				return projects.find((project) => project.id === args.id);
+				return Project.findById(args.id);
 			},
 		},
 		clients: {
 			type: new GraphQLList(ClientType),
 			resolve(parent, args) {
-				console.log({ parent });
-				return clients;
+				return Client.find();
 			},
 		},
 		client: {
 			type: ClientType,
 			args: { id: { type: GraphQLID } },
 			resolve(parent, args) {
-				return clients.find((client) => client.id === args.id);
+				return Client.findById(args.id);
 			},
 		},
 	},
